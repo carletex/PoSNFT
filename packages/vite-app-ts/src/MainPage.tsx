bcfb92c01b6231a4e2414d32486efe9784a52738
Carlos Sánchez <oceanrdn@gmail.com>
2022-06-01 16:09:58 +0200

    Initial commit

---
+++ packages/vite-app-ts/src/MainPage.tsx
@@ -0,0 +1,157 @@
+import '~~/styles/main-page.css';
+import { NETWORKS } from '@scaffold-eth/common/src/constants';
+import { GenericContract } from 'eth-components/ant/generic-contract';
+import { useContractReader, useBalance, useEthersAdaptorFromProviderOrSigners, useEventListener } from 'eth-hooks';
+import { useEthersAppContext } from 'eth-hooks/context';
+import { useDexEthPrice } from 'eth-hooks/dapps';
+import { asEthersAdaptor } from 'eth-hooks/functions';
+import React, { FC, useEffect, useState } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import { MainPageFooter, MainPageHeader, createPagesAndTabs, TContractPageList } from './components/main';
+
+import { useAppContracts, useConnectAppContracts, useLoadAppContracts } from '~~/components/contractContext';
+import { useCreateAntNotificationHolder } from '~~/components/main/hooks/useAntNotification';
+import { useBurnerFallback } from '~~/components/main/hooks/useBurnerFallback';
+import { useScaffoldProviders as useScaffoldAppProviders } from '~~/components/main/hooks/useScaffoldAppProviders';
+import { BURNER_FALLBACK_ENABLED, MAINNET_PROVIDER } from '~~/config/app.config';
+
+/**
+ * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
+ * See config/app.config.ts for configuration, such as TARGET_NETWORK
+ * See appContracts.config.ts and externalContracts.config.ts to configure your contracts
+ * See pageList variable below to configure your pages
+ * See web3Modal.config.ts to configure the web3 modal
+ * ⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️⛳️
+ *
+ * For more
+ */
+
+/**
+ * The main component
+ * @returns
+ */
+export const MainPage: FC = () => {
+  const notificationHolder = useCreateAntNotificationHolder();
+  // -----------------------------
+  // Providers, signers & wallets
+  // -----------------------------
+  // 🛰 providers
+  // see useLoadProviders.ts for everything to do with loading the right providers
+  const scaffoldAppProviders = useScaffoldAppProviders();
+
+  // 🦊 Get your web3 ethers context from current providers
+  const ethersAppContext = useEthersAppContext();
+
+  // if no user is found use a burner wallet on localhost as fallback if enabled
+  useBurnerFallback(scaffoldAppProviders, BURNER_FALLBACK_ENABLED);
+
+  // -----------------------------
+  // Load Contracts
+  // -----------------------------
+  // 🛻 load contracts
+  useLoadAppContracts();
+  // 🏭 connect to contracts for mainnet network & signer
+  const [mainnetAdaptor] = useEthersAdaptorFromProviderOrSigners(MAINNET_PROVIDER);
+  useConnectAppContracts(mainnetAdaptor);
+  // 🏭 connec to  contracts for current network & signer
+  useConnectAppContracts(asEthersAdaptor(ethersAppContext));
+
+  // -----------------------------
+  // These are the contracts!
+  // -----------------------------
+
+  // init contracts
+  const yourContract = useAppContracts('YourContract', ethersAppContext.chainId);
+  const yourNFT = useAppContracts('YourNFT', ethersAppContext.chainId);
+  const mainnetDai = useAppContracts('DAI', NETWORKS.mainnet.chainId);
+
+  // keep track of a variable from the contract in the local React state:
+  const [purpose, update] = useContractReader(
+    yourContract,
+    yourContract?.purpose,
+    [],
+    yourContract?.filters.SetPurpose()
+  );
+
+  // 📟 Listen for broadcast events
+  const [setPurposeEvents] = useEventListener(yourContract, 'SetPurpose', 0);
+
+  // -----------------------------
+  // .... 🎇 End of examples
+  // -----------------------------
+  // 💵 This hook will get the price of ETH from 🦄 Uniswap:
+  const [ethPrice] = useDexEthPrice(scaffoldAppProviders.mainnetAdaptor?.provider, scaffoldAppProviders.targetNetwork);
+
+  // 💰 this hook will get your balance
+  const [yourCurrentBalance] = useBalance(ethersAppContext.account);
+
+  const [route, setRoute] = useState<string>('');
+  useEffect(() => {
+    setRoute(window.location.pathname);
+  }, [setRoute]);
+
+  // -----------------------------
+  // 📃 Page List
+  // -----------------------------
+  // This is the list of tabs and their contents
+  const pageList: TContractPageList = {
+    mainPage: {
+      name: 'YourContract',
+      content: (
+        <GenericContract
+          contractName="YourContract"
+          contract={yourContract}
+          mainnetAdaptor={scaffoldAppProviders.mainnetAdaptor}
+          blockExplorer={scaffoldAppProviders.targetNetwork.blockExplorer}
+        />
+      ),
+    },
+    pages: [
+      {
+        name: 'YourNFT',
+        content: (
+          <GenericContract
+            contractName="YourNFT"
+            contract={yourNFT}
+            mainnetAdaptor={scaffoldAppProviders.mainnetAdaptor}
+            blockExplorer={scaffoldAppProviders.targetNetwork.blockExplorer}></GenericContract>
+        ),
+      },
+      {
+        name: 'Dai',
+        content: (
+          <GenericContract
+            contractName="Dai"
+            contract={mainnetDai}
+            mainnetAdaptor={scaffoldAppProviders.mainnetAdaptor}
+            blockExplorer={scaffoldAppProviders.targetNetwork.blockExplorer}
+          />
+        ),
+      },
+    ],
+  };
+  const { tabContents, tabMenu } = createPagesAndTabs(pageList, route, setRoute);
+
+  return (
+    <div className="App">
+      <MainPageHeader scaffoldAppProviders={scaffoldAppProviders} price={ethPrice} />
+      {/* Routes should be added between the <Switch> </Switch> as seen below */}
+      <BrowserRouter>
+        {tabMenu}
+        <Switch>
+          {tabContents}
+          {/* Subgraph also disabled in MainPageMenu, it does not work, see github issue https://github.com/scaffold-eth/scaffold-eth-typescript/issues/48! */}
+          {/*
+          <Route path="/subgraph">
+            <Subgraph subgraphUri={subgraphUri} mainnetProvider={scaffoldAppProviders.mainnetAdaptor?.provider} />
+          </Route>
+          */}
+        </Switch>
+      </BrowserRouter>
+
+      <MainPageFooter scaffoldAppProviders={scaffoldAppProviders} price={ethPrice} />
+      <div style={{ position: 'absolute' }}>{notificationHolder}</div>
+    </div>
+  );
+};
