68fe72b8636703f8635c256a07f0869f37a0d0db
Carlos Sánchez <oceanrdn@gmail.com>
2022-06-06 11:04:00 +0200

    Remove first / last block liimts. Add total counter

--- packages/hardhat-ts/contracts/PoSNFT.sol
+++ packages/hardhat-ts/contracts/PoSNFT.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 interface IPosBlockOracle {
   function getFirstRegisteredPosBlock() external view returns (uint256);
@@ -12,13 +13,11 @@ interface IPosBlockOracle {
 // ToDo. SVG Generation on tokenURI
 // ToDo. Metadata on chain generation
 contract PosNFT is ERC721, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter public totalCounter;
+
   address public PosBlockOracleAddress;
   address public buidlGuidl = 0x97843608a00e2bbc75ab0C1911387E002565DEDE;
-
-  // ToDo. July?
-  uint256 public constant FIRST_BLOCK = 14885900;
-  // ToDo. December?
-  uint256 public constant LAST_BLOCK = 14905900;
 
   constructor(address _PosBlockOracleAddress) ERC721("PosNFT", "PNFT") {
     PosBlockOracleAddress = _PosBlockOracleAddress;
@@ -29,8 +28,8 @@ contract PosNFT is ERC721, Ownable {
   }
 
   function mint(address _to, uint256 _blockNumber) public {
-    require(_blockNumber >= FIRST_BLOCK && _blockNumber <= LAST_BLOCK, "Block number off limits");
     _mint(_to, _blockNumber);
+    totalCounter.increment();
   }
 
   function claim() public {
@@ -52,23 +51,11 @@ contract PosNFT is ERC721, Ownable {
       return super.ownerOf(oracleFirstPosBlock);
     }
 
-    // Before match. The merge happened before FIRST_BLOCK
-    // (the first tokenID, representing a blockNumber, we allowed to mint).
-    if (oracleFirstPosBlock <= FIRST_BLOCK) {
-      return super.ownerOf(FIRST_BLOCK);
-    }
-
-    // After match. The merge happened after LAST_BLOCK.
-    // (the last tokenID, representing a blockNumber, we allowed to mint).
-    if (oracleFirstPosBlock >= LAST_BLOCK) {
-      return super.ownerOf(LAST_BLOCK);
-    }
-
-    // In-between match => search for closest.
+    // No match => search for closest.
     uint256 indexShift = 0;
     // ToDo. Handle equal distance?. RN the closest below wins on equal distance.
-    // Loop until we find the winner.
-    while (oracleFirstPosBlock - indexShift > FIRST_BLOCK && oracleFirstPosBlock + indexShift < LAST_BLOCK) {
+    // Loop until we find the winner. 60000 blocks ~ 10 days
+    while (indexShift < 60000) {
       indexShift++;
       if (super._exists(oracleFirstPosBlock - indexShift)) {
         return super.ownerOf(oracleFirstPosBlock - indexShift);
@@ -79,7 +66,6 @@ contract PosNFT is ERC721, Ownable {
       }
     }
 
-    // Should only happen if we sell 0 NFTs :)
     revert("No winners");
   }
 
--- packages/hardhat-ts/test/PosNFT.test.ts
+++ packages/hardhat-ts/test/PosNFT.test.ts
@@ -23,34 +23,24 @@ describe('PosNFT', function () {
     PosNFTContract = await factory.deploy(mockPosBlockOracle.address);
   });
 
-  it('Should revert when trying to MINT an off-limit blockNumber NFT', async () => {
-    const { user1 } = await getHardhatSigners(hre);
-
-    const firstBlock = await PosNFTContract.FIRST_BLOCK();
-    const lastBlock = await PosNFTContract.LAST_BLOCK();
-
-    await expect(PosNFTContract.mint(user1.address, firstBlock.sub(1))).to.be.reverted;
-    await expect(PosNFTContract.mint(user1.address, lastBlock.add(1))).to.be.reverted;
-  });
-
   it('Should allow to MINT a valid blockNumber NFT', async () => {
     const { user1 } = await getHardhatSigners(hre);
 
-    const firstBlock = await PosNFTContract.FIRST_BLOCK();
-    const mintTx = await PosNFTContract.mint(user1.address, firstBlock);
+    const blockNumber = 14914072;
+    const mintTx = await PosNFTContract.mint(user1.address, blockNumber);
     await mintTx.wait();
 
-    expect(await PosNFTContract.ownerOf(firstBlock)).to.equal(user1.address);
+    expect(await PosNFTContract.ownerOf(blockNumber)).to.equal(user1.address);
   });
 
   it('Should revert if trying to MINT a repeated blockNumber NFT', async () => {
     const { user1 } = await getHardhatSigners(hre);
 
-    const firstBlock = await PosNFTContract.FIRST_BLOCK();
-    const mintTx = await PosNFTContract.mint(user1.address, firstBlock);
+    const blockNumber = 14914072;
+    const mintTx = await PosNFTContract.mint(user1.address, blockNumber);
     await mintTx.wait();
 
-    await expect(PosNFTContract.mint(user1.address, firstBlock)).to.be.reverted;
+    await expect(PosNFTContract.mint(user1.address, blockNumber)).to.be.reverted;
   });
 
   it('Should revert if the PoS block is not set on the Oracle (getWinner)', async () => {
@@ -62,45 +52,20 @@ describe('PosNFT', function () {
   it('Should get the winner (exact match)', async () => {
     const { user1 } = await getHardhatSigners(hre);
 
-    const mintBlockNumber = (await PosNFTContract.LAST_BLOCK()).sub(100);
-    const mintTx = await PosNFTContract.mint(user1.address, mintBlockNumber);
+    const blockNumber = 14914072;
+    const mintTx = await PosNFTContract.mint(user1.address, blockNumber);
     await mintTx.wait();
 
-    await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(mintBlockNumber);
+    await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(blockNumber);
 
     expect(await PosNFTContract._getWinner()).to.be.equal(user1.address);
   });
 
-  it('Should get the winner (before match)', async () => {
-    const { user1 } = await getHardhatSigners(hre);
-
-    const firstBlock = await PosNFTContract.FIRST_BLOCK();
-    const mintTx = await PosNFTContract.mint(user1.address, firstBlock);
-    await mintTx.wait();
-
-    await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(firstBlock.sub(100));
-
-    expect(await PosNFTContract._getWinner()).to.be.equal(user1.address);
-  });
-
-  it('Should get the winner (after match)', async () => {
-    const { user1 } = await getHardhatSigners(hre);
-
-    const lastBlock = await PosNFTContract.LAST_BLOCK();
-    const mintTx = await PosNFTContract.mint(user1.address, lastBlock);
-    await mintTx.wait();
-
-    await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(lastBlock.add(100));
-
-    expect(await PosNFTContract._getWinner()).to.be.equal(user1.address);
-  });
-
-  it('Should get the winner (in between no exact match)', async () => {
+  it('Should get the winner (no exact match)', async () => {
     const { user1, user2 } = await getHardhatSigners(hre);
 
-    const lastBlock = await PosNFTContract.LAST_BLOCK();
-    const user1BlockSelection = lastBlock.sub(100);
-    const user2BlockSelection = lastBlock.sub(200);
+    const user1BlockSelection = 14914100;
+    const user2BlockSelection = 14914000;
 
     const mintUser1Tx = await PosNFTContract.mint(user1.address, user1BlockSelection);
     await mintUser1Tx.wait();
@@ -108,7 +73,7 @@ describe('PosNFT', function () {
     const mintUser2Tx = await PosNFTContract.mint(user2.address, user2BlockSelection);
     await mintUser2Tx.wait();
 
-    const winnerBlock = lastBlock.sub(160);
+    const winnerBlock = 14914040;
     await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(winnerBlock);
 
     expect(await PosNFTContract._getWinner()).to.be.equal(user2.address);
@@ -123,9 +88,8 @@ describe('PosNFT', function () {
   it('Should pay the winner & BG on a valid claim', async () => {
     const { user1, user2 } = await getHardhatSigners(hre);
 
-    const lastBlock = await PosNFTContract.LAST_BLOCK();
-    const user1BlockSelection = lastBlock.sub(100);
-    const user2BlockSelection = lastBlock.sub(200);
+    const user1BlockSelection = 14914100;
+    const user2BlockSelection = 14914000;
 
     const mintUser1Tx = await PosNFTContract.mint(user1.address, user1BlockSelection);
     await mintUser1Tx.wait();
@@ -133,7 +97,7 @@ describe('PosNFT', function () {
     const mintUser2Tx = await PosNFTContract.mint(user2.address, user2BlockSelection);
     await mintUser2Tx.wait();
 
-    const winnerBlock = lastBlock.sub(160);
+    const winnerBlock = 14914040;
     await mockPosBlockOracle.mock.getFirstRegisteredPosBlock.returns(winnerBlock);
 
     // Send some ETH to the contract
